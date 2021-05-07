d8b8b260a71be147238c2089e935b5f0f55bfa27
ryucxd <ryucxd@gmail.com>
2021-05-07 08:17:25 +0100

    added month/year picker. it isnt needed for a while so publishing and leaving this alone

--- slimline_btp_dop/frmMainMenu.Designer.cs
+++ slimline_btp_dop/frmMainMenu.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnExcel = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbMonthStart = new System.Windows.Forms.ComboBox();
+            this.cmbYearStart = new System.Windows.Forms.ComboBox();
+            this.cmbYearEnd = new System.Windows.Forms.ComboBox();
+            this.cmbMonthEnd = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,11 +69,87 @@
             this.btnExcel.UseVisualStyleBackColor = true;
             this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(240, 31);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(240, 31);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.label1.Location = new System.Drawing.Point(139, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "to";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(446, 28);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(76, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cmbMonthStart
+            // 
+            this.cmbMonthStart.FormattingEnabled = true;
+            this.cmbMonthStart.Location = new System.Drawing.Point(12, 8);
+            this.cmbMonthStart.Name = "cmbMonthStart";
+            this.cmbMonthStart.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonthStart.TabIndex = 6;
+            // 
+            // cmbYearStart
+            // 
+            this.cmbYearStart.FormattingEnabled = true;
+            this.cmbYearStart.Location = new System.Drawing.Point(12, 33);
+            this.cmbYearStart.Name = "cmbYearStart";
+            this.cmbYearStart.Size = new System.Drawing.Size(121, 21);
+            this.cmbYearStart.TabIndex = 7;
+            // 
+            // cmbYearEnd
+            // 
+            this.cmbYearEnd.FormattingEnabled = true;
+            this.cmbYearEnd.Location = new System.Drawing.Point(158, 33);
+            this.cmbYearEnd.Name = "cmbYearEnd";
+            this.cmbYearEnd.Size = new System.Drawing.Size(121, 21);
+            this.cmbYearEnd.TabIndex = 9;
+            // 
+            // cmbMonthEnd
+            // 
+            this.cmbMonthEnd.FormattingEnabled = true;
+            this.cmbMonthEnd.Location = new System.Drawing.Point(158, 8);
+            this.cmbMonthEnd.Name = "cmbMonthEnd";
+            this.cmbMonthEnd.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonthEnd.TabIndex = 8;
+            // 
             // frmMainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 450);
+            this.Controls.Add(this.cmbYearEnd);
+            this.Controls.Add(this.cmbMonthEnd);
+            this.Controls.Add(this.cmbYearStart);
+            this.Controls.Add(this.cmbMonthStart);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.btnExcel);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmMainMenu";
@@ -73,6 +157,7 @@
             this.Text = "btp dop";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,6 +165,14 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbMonthStart;
+        private System.Windows.Forms.ComboBox cmbYearStart;
+        private System.Windows.Forms.ComboBox cmbYearEnd;
+        private System.Windows.Forms.ComboBox cmbMonthEnd;
     }
 }
 
--- slimline_btp_dop/frmMainMenu.cs
+++ slimline_btp_dop/frmMainMenu.cs
@@ -16,68 +16,37 @@ namespace slimline_btp_dop
         public frmMainMenu()
         {
             InitializeComponent();
-            string temp = "";
-            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
-            {
-                conn.Open();
-                using (SqlCommand cmd = new SqlCommand("usp_btp_dop_data", conn))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
+            cmbMonthStart.Items.Add("Janurary");
+            cmbMonthStart.Items.Add("February");
+            cmbMonthStart.Items.Add("March");
+            cmbMonthStart.Items.Add("April");
+            cmbMonthStart.Items.Add("May");
+            cmbMonthStart.Items.Add("June");
+            cmbMonthStart.Items.Add("July");
+            cmbMonthStart.Items.Add("August");
+            cmbMonthStart.Items.Add("September");
+            cmbMonthStart.Items.Add("October");
+            cmbMonthStart.Items.Add("November");
+            cmbMonthStart.Items.Add("December");
 
-                    SqlDataAdapter da = new SqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-                    //start messing with data here, add up totals for packed/invoice
-                    int counter = dt.Rows.Count + 4;
-                    for (int i = dt.Rows.Count; i < counter ; i++) //insert 3 rows ~ one for line break then total packed/invoiced
-                    {
-                        DataRow newRow = dt.NewRow();
-                        dt.Rows.InsertAt(newRow, i);
-                    }
+            cmbYearStart.Items.Add("2018");
+            cmbYearStart.Items.Add("2019");
+            cmbYearStart.Items.Add("2020");
+            cmbYearStart.Items.Add("2021");
+            cmbYearStart.Items.Add("2022");
+            cmbYearStart.Items.Add("2023");
+            cmbYearStart.Items.Add("2024");
+            cmbYearStart.Items.Add("2025");
+            cmbYearStart.Items.Add("2026");
+            for (int i = 0; i < cmbMonthStart.Items.Count; i++)
+                cmbMonthEnd.Items.Add(cmbMonthStart.Items[i]);
+            for (int i = 0; i < cmbYearStart.Items.Count; i++)
+                cmbYearEnd.Items.Add(cmbYearStart.Items[i]);
 
-                    dt.Rows[dt.Rows.Count - 3][0] = "TOTAL PACKED:";
-                    dt.Rows[dt.Rows.Count - 2][0] = "TOTAL INVOICED:";
-                    dt.Rows[dt.Rows.Count - 1][0] = "SUM:";
-
-                    //DATA STARTS AT COLUMN 4
-
-                    for (int column = 4; column < dt.Columns.Count;column++) //each column
-                    {
-                        double packed = 0;
-                        double invoiced = 0;
-                        for (int row = 0; row < dt.Rows.Count - 4;row++)
-                        {
-                            if (dt.Rows[row][0].ToString() == "INVOICED") //check for invoice vs packed
-                            {
-                                if (String.IsNullOrEmpty(dt.Rows[row][column].ToString()) == false)
-                                    invoiced = invoiced + Convert.ToDouble(dt.Rows[row][column].ToString());
-                            }
-                            if (dt.Rows[row][0].ToString() == "PACKED") 
-                            {
-                                if (String.IsNullOrEmpty(dt.Rows[row][column].ToString()) == false)
-                                    packed = packed + Convert.ToDouble(dt.Rows[row][column].ToString());
-                            }
-
-                        }
-                        //now add them to the last two rows
-                        dt.Rows[dt.Rows.Count - 3][column] = packed.ToString();
-                        dt.Rows[dt.Rows.Count - 2][column] =  invoiced.ToString();
-                        dt.Rows[dt.Rows.Count - 1][column] =  (packed - invoiced).ToString();
-
-                    }
-
-
-                    dataGridView1.DataSource = dt;
-                    foreach (DataGridViewColumn column in dataGridView1.Columns)
-                    {
-                        column.SortMode = DataGridViewColumnSortMode.NotSortable; // dont allow the user to sort the columns, will cause big problems 
-                        column.DefaultCellStyle.BackColor = Color.DeepSkyBlue;
-                    }
-
-                }
-                conn.Close();
-            
-            }
+            cmbMonthStart.Text = DateTime.Now.ToString("MMMM");
+            cmbYearStart.Text = DateTime.Now.AddYears(-1).ToString("yyyy");
+            cmbMonthEnd.Text = DateTime.Now.ToString("MMMM");
+            cmbYearEnd.Text = DateTime.Now.ToString("yyyy");
         }
 
         private void btnExcel_Click(object sender, EventArgs e) //this highlights all of the dgv, copies it, then pastes into a new excel instance
@@ -103,8 +72,8 @@ namespace slimline_btp_dop
             xlWorkSheet.Name = "test";
 
             //              VVV this changes backcolour 
-            int row = dataGridView1.Rows.Count  + 1; //how many rows down the  sum field is
-            for (int column = 5; column < dataGridView1.Columns.Count  + 1; column++)
+            int row = dataGridView1.Rows.Count + 1; //how many rows down the  sum field is
+            for (int column = 5; column < dataGridView1.Columns.Count + 1; column++)
             {
                 //read the cell value
 
@@ -117,5 +86,86 @@ namespace slimline_btp_dop
             }
         }
 
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            int monthCounter = 0;
+            monthCounter = Convert.ToInt32(cmbMonthStart.SelectedIndex.ToString()) + 1;
+            if (monthCounter < 10)
+                temp = cmbYearStart.Text.ToString() + "/0" + monthCounter.ToString() + "/01";
+            else
+                temp = cmbYearStart.Text.ToString()  + "/" + monthCounter.ToString() + "/01";
+            DateTime startDate = Convert.ToDateTime(temp);
+
+            monthCounter = Convert.ToInt32(cmbMonthEnd.SelectedIndex.ToString()) + 1;
+            if (monthCounter < 10)
+                temp = cmbYearEnd.Text.ToString() + "/0" + monthCounter + "/01";
+            else
+                temp = cmbYearEnd.Text.ToString() + "/" + monthCounter + "/01";
+
+            DateTime endDate = Convert.ToDateTime(temp);
+            endDate = endDate.AddMonths(1).AddDays(-1);
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("usp_btp_dop_data", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@start", SqlDbType.Date).Value = startDate;
+                    cmd.Parameters.Add("@end", SqlDbType.Date).Value = endDate;
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    //start messing with data here, add up totals for packed/invoice
+                    int counter = dt.Rows.Count + 4;
+                    for (int i = dt.Rows.Count; i < counter; i++) //insert 3 rows ~ one for line break then total packed/invoiced
+                    {
+                        DataRow newRow = dt.NewRow();
+                        dt.Rows.InsertAt(newRow, i);
+                    }
+
+                    dt.Rows[dt.Rows.Count - 3][0] = "TOTAL PACKED:";
+                    dt.Rows[dt.Rows.Count - 2][0] = "TOTAL INVOICED:";
+                    dt.Rows[dt.Rows.Count - 1][0] = "SUM:";
+
+                    //DATA STARTS AT COLUMN 4
+
+                    for (int column = 4; column < dt.Columns.Count; column++) //each column
+                    {
+                        double packed = 0;
+                        double invoiced = 0;
+                        for (int row = 0; row < dt.Rows.Count - 4; row++)
+                        {
+                            if (dt.Rows[row][0].ToString() == "INVOICED") //check for invoice vs packed
+                            {
+                                if (String.IsNullOrEmpty(dt.Rows[row][column].ToString()) == false)
+                                    invoiced = invoiced + Convert.ToDouble(dt.Rows[row][column].ToString());
+                            }
+                            if (dt.Rows[row][0].ToString() == "PACKED")
+                            {
+                                if (String.IsNullOrEmpty(dt.Rows[row][column].ToString()) == false)
+                                    packed = packed + Convert.ToDouble(dt.Rows[row][column].ToString());
+                            }
+
+                        }
+                        //now add them to the last two rows
+                        dt.Rows[dt.Rows.Count - 3][column] = packed.ToString();
+                        dt.Rows[dt.Rows.Count - 2][column] = invoiced.ToString();
+                        dt.Rows[dt.Rows.Count - 1][column] = (packed - invoiced).ToString();
+
+                    }
+
+
+                    dataGridView1.DataSource = dt;
+                    foreach (DataGridViewColumn column in dataGridView1.Columns)
+                        column.SortMode = DataGridViewColumnSortMode.NotSortable; // dont allow the user to sort the columns, will cause big problems 
+
+
+                }
+                conn.Close();
+
+            }
+        }
     }
 }
